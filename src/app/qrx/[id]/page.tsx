b4abb8b62b8c674f394306fe85eb7061503f5677
Minh Huynh <mhhuynh@gmx.de>
2026-01-12 23:29:52 +0100

    Fix app router layout and qrx route

---
+++ src/app/qrx/[id]/page.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "mioseg qr",
+  description: "QR-X Viewer",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="de">
+      <body>{children}</body>
+    </html>
+  );
+}
